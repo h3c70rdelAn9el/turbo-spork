53a218a164ca4c746984b7b489ab6d08783918a0
Hector del Angel <hectordelangel@gmail.com>
2021-04-08 11:48:14 -0700

    quick styling to home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    home
+  <div class="h-screen flex">
+    <div class="mx-auto my-auto text-center">
+      <h1 class="text-3xl">Vue 3 practice</h1>
+      <p>Some some basic quick apps using vue 3 to practice 💼</p>
+    </div>
   </div>
 </template>
