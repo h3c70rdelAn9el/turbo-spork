5892e205e1552b50b622673f1b9a570ca3fd5f9d
Hector del Angel <hectordelangel@gmail.com>
2021-05-24 09:51:54 -0700

    add chat app

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/database'
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASEURL,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
   storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
@@ -13,5 +13,8 @@ const firebaseConfig = {
   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENTID,
 }
 firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+export const chatsRef = db.ref('chats')
 
 export default firebase;
