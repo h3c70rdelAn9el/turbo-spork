83741f8611e6650bb4da944190ab64526b9766c7
Hector del Angel <hectordelangel@gmail.com>
2021-06-02 11:54:49 -0700

    add some styling to edit page.

--- src/pages/EditTodo.vue
+++ src/pages/EditTodo.vue
@@ -1,52 +1,54 @@
 <template>
-  <div>
-    Edit Todo
-    <form @submit.prevent="update">
-          <div class="p-2">
-            <label>Title</label>
-            <input 
-              class="w-full p-2 rounded border"
-              v-model="form.title"
+  <div class="flex flex-col h-full w-full">
+    <h2 class="text-center text-3xl">Edit Todo</h2>
+    <form @submit.prevent="update" class="m-auto bg-gray-200 w-1/2 md:w-1/3 border rounded-md shadow-lg">
+      <div class="p-2">
+        <div class="flex flex-col">
+          <label class="text-sm">Title</label>
+          <input class="w-full p-2 rounded border" v-model="form.title" />
+        </div>
+        <div class="flex">
+          <button class="p-2 mx-auto">
+            <input
+              class="w-full p-2 rounded-md border shadow hover:bg-gray-300 cursor-pointer"
+              type="submit"
             />
-            <button class="p-2">
-              <input 
-                class="w-full p-2 rounded-md border shadow hover:bg-gray-300"
-                type="submit"
-              />
-            </button>
-          </div>
+          </button>
+
+        </div>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-import { reactive, onMounted, computed } from 'vue'
-import { getTodo, updateTodo } from '../utilities/firebase'
-import { useRoute, useRouter } from 'vue-router'
+  import { reactive, onMounted, computed } from 'vue'
+  import { getTodo, updateTodo } from '../utilities/firebase'
+  import { useRoute, useRouter } from 'vue-router'
 
-export default {
-  setup() {
-    const route = useRoute()
-    const router = useRouter()
-    const todoId = computed(() => route.params.id)
-    const form = reactive({
-      title: ''
-    })
-    onMounted(async () => {
-      const todo = await getTodo(todoId.value)
-      console.log(todo, todoId.value)
-      form.title = todo.title
-    })
+  export default {
+    setup() {
+      const route = useRoute()
+      const router = useRouter()
+      const todoId = computed(() => route.params.id)
+      const form = reactive({
+        title: '',
+      })
+      onMounted(async () => {
+        const todo = await getTodo(todoId.value)
+        console.log(todo, todoId.value)
+        form.title = todo.title
+      })
 
-    const update = async () => {
-      await updateTodo(todoId.value, { ...form })
-      router.push('/todos')
-      form.title = ''
-    }
-    return {
-      form,
-      update
-    }
+      const update = async () => {
+        await updateTodo(todoId.value, { ...form })
+        router.push('/todos')
+        form.title = ''
+      }
+      return {
+        form,
+        update,
+      }
+    },
   }
-}
 </script>
--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,28 +1,32 @@
 <template>
-  <section class="flex md:w-full">
-    Todo
-    <div class="m-auto">
-      <div class="flex flex-wrap justify-center w-full">
+  <section class="flex w-full">
+    <div class="mx-auto">
+      <div class="flex flex-wrap justify-center">
         <div class="mt-10">
           <AddTodoModal />
-          <table>
+          <table class="w-full">
             <thead>
               <tr>
-                <th class="px-4 py-1 border border-gray-300">TODO</th>
-                <th class="px-4 py-1 border border-gray-300">ACTIONS</th>
+                <th class="px-5 py-1 border-b border-gray-300">TODO</th>
+                <th class="px-5 py-1 border-b border-gray-300">ACTIONS</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="{ id, title } in todos" :key="id" class="border-b px-2 py-1">
+              <tr
+                v-for="{ id, title } in todos"
+                :key="id"
+                class="border-b px-2 py-1"
+              >
                 <!-- <td>{{ todo.title }}</td> -->
                 <td>{{ title }}</td>
                 <td class="flex justify-center">
-                   <router-link :to="`/edit/${id}`">
-                  <button class="border rounded-md px-2 py-1 shadow-lg mx-auto">
-                    Edit
-                  </button>
-             
-            </router-link>
+                  <router-link :to="`/edit/${id}`">
+                    <button
+                      class="border rounded-md px-2 py-1 shadow-lg mx-auto"
+                    >
+                      Edit
+                    </button>
+                  </router-link>
                 </td>
               </tr>
             </tbody>
@@ -30,43 +34,43 @@
         </div>
       </div>
     </div>
-  </section> 
+  </section>
 </template>
 
 <script>
-// import EditTodoModal from '../components/Todo/EditTodoModal.vue'
-// import { reactive } from 'vue'
-// import axios from 'axios'
-import { useLoadTodos } from '../utilities/firebase'
-import AddTodoModal from '../components/Todo/AddTodoModal.vue'
+  // import EditTodoModal from '../components/Todo/EditTodoModal.vue'
+  // import { reactive } from 'vue'
+  // import axios from 'axios'
+  import { useLoadTodos } from '../utilities/firebase'
+  import AddTodoModal from '../components/Todo/AddTodoModal.vue'
 
-export default {
-  components: { 
-    AddTodoModal
-  },
-  // setup() {
-  //   const state = reactive({
-  //     todos: []
-  //   })
+  export default {
+    components: {
+      AddTodoModal,
+    },
+    // setup() {
+    //   const state = reactive({
+    //     todos: []
+    //   })
 
-  //   onMounted(async () => {
-  //     const { data } = await axios.get(`/todos`)
-  //   })
-  // }
-  // setup() {
-  //   const form = reactive({ title: '' })
-    
-  //   const onSubmit = async () => {
-  //     await createTodo({ ...form })
-  //     form.title = ''
-  //   }
-    
-  //   return { onSubmit, form }
-  // }
-  setup() {
-    const todos = useLoadTodos()
-    // const createTodo = createTodo()
-    return { todos }
+    //   onMounted(async () => {
+    //     const { data } = await axios.get(`/todos`)
+    //   })
+    // }
+    // setup() {
+    //   const form = reactive({ title: '' })
+
+    //   const onSubmit = async () => {
+    //     await createTodo({ ...form })
+    //     form.title = ''
+    //   }
+
+    //   return { onSubmit, form }
+    // }
+    setup() {
+      const todos = useLoadTodos()
+      // const createTodo = createTodo()
+      return { todos }
+    },
   }
-}
 </script>
